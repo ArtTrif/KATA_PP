c2a43a8308374acc9bf43390e1af1f5680126e4e
ArtTrif <arttrif@yandex.ru>
2022-03-02 10:22:24 +0700

    JavaPP_1.1.3

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -43,27 +43,27 @@ public class UserDaoJDBCImpl implements UserDao {
     public void saveUser(String name, String lastName, byte age) {
 
 
-        try {
+        /*try {
             Statement statement = connection.createStatement();
             statement.executeUpdate("INSERT INTO table_users (name, lastName, age) values ('" + name + "','" + lastName + "'," + age + ");");
             System.out.println("User с именем - " + name + " добавлен в базу данных");
         } catch (SQLException e) {
             e.printStackTrace();
-        }
+        }*/
         //чтобы избежать SQL инъекций напишем подругому как ниже
 
 
-        /*try {
-            PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO table_users VALUES (1,?,?,?)");
-
+        try (PreparedStatement preparedStatement =
+                     connection.prepareStatement("INSERT INTO table_users(name, lastName, age) VALUES (?,?,?);")){
             preparedStatement.setString(1, name);
             preparedStatement.setString(2, lastName);
             preparedStatement.setByte(3, age);
             preparedStatement.executeUpdate();
-            System.out.println("User добавлен");
+            System.out.println("User с именем - " + name + " добавлен в базу данных");
         } catch (SQLException e) {
             e.printStackTrace();
-        }*/
+        }
+
 
     }
 
