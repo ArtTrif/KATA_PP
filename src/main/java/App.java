6a22d08fd0024048d0f1e4c2f009efd84648579c
ArtTrif <arttrif@yandex.ru>
2022-03-06 01:52:47 +0700

    Spring_HelloWorld

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,4 @@
+import Cat.Cat;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -7,6 +8,13 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat cat = (Cat) applicationContext.getBean("cat");
+        Cat cat2 = (Cat) applicationContext.getBean("cat");
         System.out.println(bean.getMessage());
+        System.out.println("пара HelloWorld: " + (bean == bean2));
+        System.out.println("пара Cat: " + (cat == cat2));
     }
 }
